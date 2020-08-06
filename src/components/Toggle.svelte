2f78715718213bb8a5b9f6ee6b00535b243b9c29
Kevin Allison <kvnallsn@gmail.com>
2020-08-06 07:46:58 -0500

    new toggle components:
    Toggle: Generic Toggle
    TwoItemToggle: Toggles between two named items

---
+++ src/components/Toggle.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+  // npm imports
+
+  // props
+  export let id: string = "toggle";
+  export let name: string = "toggle";
+ 
+  // local mutable data
+  let toggle: boolean = false;
+</script>
+
+<style>
+  .label-on {
+    @apply translate-x-full border-primary-400;
+  }
+
+  .label-off {
+    @apply translate-x-0 border-gray-400;
+  }
+</style>
+
+<div
+  class="relative rounded-full w-12 h-6 transition duration-200 ease-linear"
+  class:bg-primary-400={toggle}
+  class:bg-gray-400={!toggle}
+>
+  <label
+    for="toggle"
+    class="absolute left-0 bg-white border-2 mb-2 w-6 h-6 rounded-full transition transform duration-100 ease-linear cursor-pointer"
+    class:label-on={toggle}
+    class:label-off={!toggle}
+  >
+
+  <input
+    {id}
+    {name}
+    type="checkbox"
+    class="appearance-none w-full h-full active:outline-none focus:outline-none"
+    on:click="{_e => toggle = !toggle}"
+  >
+</div>
---
+++ src/components/TwoItemToggle.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+  // npm imports
+
+  // props
+  export let left: string;
+  export let right: string;
+  export let value = left;
+ 
+  // local mutable data
+  let toggle: boolean = false;
+
+  function onToggleClick() {
+    toggle = !toggle;
+    value = toggle ? right : left;
+  }
+</script>
+
+<style>
+  .btn {
+    @apply inline-flex items-center transition-colors duration-300 ease-in rounded-l-full px-3 py-1 text-sm;
+  }
+
+  .btn:hover {
+    @apply text-primary-400;
+  }
+
+  .btn:focus {
+    @apply outline-none text-primary-400;
+  }
+
+  .active {
+    @apply bg-white text-primary-400 rounded-full;
+  }
+</style>
+
+<div
+  class="bg-gray-200 text-sm text-gray-500 leading-none border-2 border-gray-200 rounded-full inline-flex"
+  >
+  <button class="btn" class:active={!toggle} on:click={onToggleClick}>
+    <span>{left}</span>
+  </button>
+
+  <button class="btn" class:active={toggle} on:click={onToggleClick}>
+    <span>{right}</span>
+  </button>
+
+</div>
