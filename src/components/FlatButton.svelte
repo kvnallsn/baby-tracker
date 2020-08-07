12ae15723d8463be348e921d71242bcd8ed8321d
Kevin Allison <kvnallsn@gmail.com>
2020-08-07 16:10:48 -0500

    new component: FlatButton

---
+++ src/components/FlatButton.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+  // Properties
+  export let text: string;                // Text to display on button, no default (required)
+  export let color: string = "primary";   // Color of this button, default is "primary".
+</script>
+
+<style>
+  .blue {
+    @apply text-blue-700;
+  }
+
+  .blue:hover {
+    @apply text-blue-500;
+  }
+
+  .blue:focus {
+    @apply text-blue-900;
+  }
+
+  .blue:active {
+    @apply text-black;
+  }
+
+  .primary {
+    @apply text-primary-700;
+  }
+
+  .primary:hover {
+    @apply text-primary-500;
+  }
+
+  .primary:focus {
+    @apply text-primary-900;
+  }
+
+  .primary:active {
+    @apply text-primary-900;
+  }
+</style>
+
+<div class="relative inline-block text-left">
+  <span class="rounded-md">
+    <button
+      type="button"
+      on:click
+      class={`inline-flex justify-center w-full rounded-md px-4 py-2
+      text-sm leading-5 font-medium focus:outline-none transition ease-in-out
+      duration-150 ${color || ''}`}
+      aria-haspopup="true"
+      aria-expanded="true">
+      {text}
+    </button>
+  </span>
+</div>
