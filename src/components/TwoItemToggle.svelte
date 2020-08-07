03cacfa034ff51766f50e2e956162c42854c145d
Kevin Allison <kvnallsn@gmail.com>
2020-08-07 08:48:42 -0500

    TwoItemToggle: emits event on click
    TwoItemToggle will now emit the label of the active side when clicked.
    The event emitted is 'click'

--- src/components/TwoItemToggle.svelte
+++ src/components/TwoItemToggle.svelte
@@ -1,30 +1,32 @@
 <script lang="ts">
   // npm imports
+  import { createEventDispatcher } from 'svelte';
 
   // props
   export let left: string;
   export let right: string;
   export let value = left;
- 
-  // local mutable data
-  let toggle: boolean = false;
+
+  // local variables
+  const dispatch = createEventDispatcher();
 
   function onToggleClick() {
-    toggle = !toggle;
-    value = toggle ? right : left;
+    value = (value === left) ? right : left;
+
+    dispatch('click', { value: value });
   }
 </script>
 
 <style>
-  .btn {
+  .toggle {
     @apply inline-flex items-center transition-colors duration-300 ease-in rounded-l-full px-3 py-1 text-sm;
   }
 
-  .btn:hover {
+  .toggle:hover {
     @apply text-primary-400;
   }
 
-  .btn:focus {
+  .toggle:focus {
     @apply outline-none text-primary-400;
   }
 
@@ -36,11 +38,11 @@
 <div
   class="bg-gray-200 text-sm text-gray-500 leading-none border-2 border-gray-200 rounded-full inline-flex"
   >
-  <button class="btn" class:active={!toggle} on:click={onToggleClick}>
+  <button class="toggle" class:active={value === left} on:click={onToggleClick}>
     <span>{left}</span>
   </button>
 
-  <button class="btn" class:active={toggle} on:click={onToggleClick}>
+  <button class="toggle" class:active={value === right} on:click={onToggleClick}>
     <span>{right}</span>
   </button>
 
